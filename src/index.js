d67882a3bd7aa3c80c2dcfc4ab26bfa7f6a5521c
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-03-15 17:50:57 +0300

    create app template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('start app')
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const root = document.getElementById('root')
+const Header = () => <h1>Header</h1>
+const Main = () => <main>Main</main>
+const Footer = () => <footer>footer</footer>
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
+
+ReactDom.render(<App />, root)
+
