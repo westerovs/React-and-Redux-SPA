098c84775a5e6c97c93a99b7ff8ef4541d6a9fe8
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-03-16 11:10:13 +0300

    refactoring styles

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -5,7 +5,7 @@ const AppHeader = ({toDo, done}) => {
     return (
         <div className="app-header d-flex">
             <h1>Todo List</h1>
-            <h2>{ toDo } more to do, { done } done</h2>
+            <h2><span>{ toDo }</span> more to do, <span>{ done }</span> done</h2>
         </div>
     );
 };
