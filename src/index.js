7093fd0a1387d043063b09a36dee2e630d8616cc
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-03-16 10:49:51 +0300

    folders from components

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Header from './components/app-header.js'
-import SearchPanel from './components/search-panel.js'
-import TodoList from './components/todo-list.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const root = document.getElementById('root')
+import App from './components/app';
 
-
-const App = () => {
-    
-    const todoData = [
-        {label: 'Выучить реакт', important: false, id: 1},
-        {label: 'Выпить кофе', important: false, id: 2},
-        {label: 'не отвлекаться', important: true, id: 3},
-    ]
-    
-    return (
-        <div>
-            <Header/>
-            <SearchPanel/>
-            <TodoList todos={ todoData }/>
-        </div>
-    )
-}
-
-ReactDom.render(<App />, root)
-
+ReactDOM.render(<App/>,
+    document.getElementById('root'));
