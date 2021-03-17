6c4ceda8f1cde6ac884d42d58a16340bf719f569
Александр <westerovs@mail.ru>
2021-03-17 23:03:03 +0300

    added form

--- src/components/added-item/added-item.js
+++ src/components/added-item/added-item.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './added-item.css';
 
 export default class AddedItem extends Component {
+    
+    
     render() {
-        
         const { onAddedItem } = this.props
         
         return (
-            <button onClick={ () => onAddedItem('hello') } className="added">Add</button>
+            <div className="todo__added">
+                <input
+                    type="text"
+                    className="todo__added-input"
+                    placeholder="enter task..."
+                />
+                <button onClick={ () => onAddedItem('hello') } className="todo__added-btn">
+                    Add
+                </button>
+            </div>
         );
     }
 }
